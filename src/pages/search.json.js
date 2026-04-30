2d44f89b354ab42abab38d0ebcfb2f562ce92998
lotrives <lotrives@icloud.com>
2026-04-30 23:56:16 +0200

    Eliminar prefijo /blog/ de las URLs de los posts

--- src/pages/search.json.js
+++ src/pages/search.json.js
@@ -8,7 +8,7 @@ function stripHtml(value = '') {
 }
 
 function postUrl(post) {
-	return `/blog/${post.id.replace(/\.md$/, '').replace(/^(\d{4})-(\d{2})-(\d{2})-(.+)$/, '$1/$2/$3/$4')}/`;
+	return `/${post.id.replace(/\.md$/, '').replace(/^(\d{4})-(\d{2})-(\d{2})-(.+)$/, '$1/$2/$3/$4')}/`;
 }
 
 export async function GET() {
