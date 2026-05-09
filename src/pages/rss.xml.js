10f824dc3f95c343ec8111121eb7786a96b5e592
lotrives <lotrives@icloud.com>
2026-05-09 22:22:25 +0200

    Añade contenido completo al feed RSS del blog

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,6 +2,25 @@ import { getCollection } from 'astro:content';
 import rss from '@astrojs/rss';
 import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
 
+const escapeHtml = (str) =>
+	String(str)
+		.replaceAll('&', '&amp;')
+		.replaceAll('<', '&lt;')
+		.replaceAll('>', '&gt;')
+		.replaceAll('"', '&quot;')
+		.replaceAll("'", '&#39;');
+
+const markdownToFeedHtml = (body) => {
+	const escaped = escapeHtml(body.trim());
+	const withLinks = escaped.replace(
+		/\[([^\]]+)\]\((https?:\/\/[^)]+)\)/g,
+		'<a href="$2">$1</a>'
+	);
+	return `<p>${withLinks
+		.replace(/\n{2,}/g, '</p><p>')
+		.replace(/\n/g, '<br />')}</p>`;
+};
+
 export async function GET(context) {
 	const posts = await getCollection('blog');
 	return rss({
@@ -19,6 +38,7 @@ export async function GET(context) {
 					title: post.data.title,
 					pubDate: post.data.pubDate,
 					description: post.data.description,
+					content: markdownToFeedHtml(post.body),
 					link,
 				};
 			}),
